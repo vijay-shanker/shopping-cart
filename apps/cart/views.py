b8461a1a9400a5f528d4d0a88b35a7acb14b3ecd
vijay-shanker <deontics@gmail.com>
2013-10-22 22:27:11 +0530

    minor fix

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -19,13 +19,13 @@ class AddToCart(DetailView):
 
         if 'CART-ID' in request.session.keys():
             cart = Cart.objects.get(cart_id = request.session['CART-ID'])
-            cart_item = CartItem.objects.create(content_type=content_type, object_id=self.kwargs['obj_id'])
+            cart_item, created = CartItem.objects.get_or_create(content_type=content_type, object_id=self.kwargs['obj_id'])
             cart.cartitems.add(cart_item)
             cart.save()
         else:
             cart_id = uuid.uuid4().hex
             cart = Cart.objects.create(cart_id=cart_id)
-            cart_item = CartItem.objects.create(content_type=content_type, object_id=self.kwargs['obj_id'])
+            cart_item, created = CartItem.objects.get_or_create(content_type=content_type, object_id=self.kwargs['obj_id'])
             cart.cartitems.add(cart_item)
             request.session['CART-ID'] = cart_id
 
